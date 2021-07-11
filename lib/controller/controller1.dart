cbb81ca116a4557dd5e6beb29391988fbf6b79b9
parmeet <parmeets834@gmail.com>
2021-07-11 21:58:05 +0530

    ios nore shs

--- lib/controller/controller1.dart
+++ lib/controller/controller1.dart
@@ -14,6 +14,7 @@ class Controller1 extends GetxController{
     // something i added here pta ni kya kya
    // kasfjasbfjasbfafmakfnjabfwejfwfwhwevfgjwvfgwefvhjvhasfgyqwgfyqwfgyuwqfyuqfhjcq
   //  qfbhqwvfhwqvfhwfhwvhfvwqf
+    // ios more test
     super.onClose();
     print("controller closed");
   }
